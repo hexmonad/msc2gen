d37035a44424080b1955557e450a04d9d3164dbd
Sergey Chernenok <chernenoksergey@gmail.com>
2014-03-12 16:40:31 +0700

    The output format of msc2gen was changed to Msc-generator format

--- src/org/hexmonad/msc2gen/Msc2Gen.java
+++ src/org/hexmonad/msc2gen/Msc2Gen.java
@@ -5,10 +5,10 @@ import org.antlr.runtime.RecognitionException;
 
 public class Msc2Gen {
     public static void main( String[] args ) {
-        String usage = "msc2gen <in_file.msc> <out_file.mscgen>\n" +
-                       " where\n" +
-                       "  <in_file.msc> - the file that represents input MSC (event-oriented syntax)\n" +
-                       "  <out_file.mscgen> - the file that is the result of translation input MSC format to format of mscgen app";
+        String usage = "msc2gen <in_file.msc> <out_file.signalling>\n" +
+                " where\n" +
+                "  <in_file.msc> - the file that represents input MSC (event-oriented syntax)\n" +
+                "  <out_file.signalling> - the file that is the result of translation input MSC format to format of Msc-generator app";
 
         if( args.length > 0 && args[0].equals("--help") ) {
             System.out.println( usage );
